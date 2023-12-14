568c7cd32c6b8a2ffde3a2530ab4ae1a1c8a399a
Valik <valik3201@gmail.com>
2023-12-15 00:55:05 +0100

    refactor: code refactor and add new functions

--- app/js/script.js
+++ app/js/script.js
@@ -1,185 +1,270 @@
-const vsCPUButton = document.querySelector(".game-menu__start-btn--vs-cpu");
+let player1Mark = "";
+let currentPlayer = "X";
+let board = Array(9).fill("");
+let playerScore = 0;
+let player1Score = 0;
+let player2Score = 0;
+let tiesScore = 0;
+let cpuScore = 0;
+let winner = null;
 
-const game = {
-  player1Mark: "", // Will store the mark (X or O) chosen by player 1
-  currentPlayer: "X", // Will track the current player (X or O)
+let gameEnded = false;
 
-  board: Array(9).fill(""),
+const gameBoard = document.querySelector(".game-board");
 
-  choosePlayerAndMark: function (playerMark) {
-    this.player1Mark = playerMark;
-    this.currentPlayer = playerMark;
+function choosePlayerAndMark(playerMark) {
+  player1Mark = playerMark;
+  currentPlayer = playerMark;
+  console.log("Метка первого игрока выбрана:", player1Mark);
+}
 
-    console.log("Метка первого игрока выбрана:", this.player1Mark);
-  },
+function renderBoard() {
+  const gameBoardField = document.querySelector(".game-board__field");
+  gameBoardField.innerHTML = "";
 
-  renderBoard: function () {
-    const gameBoardField = document.querySelector(".game-board__field");
-    gameBoardField.innerHTML = "";
+  board.forEach((_value, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("game-board__cell");
+    cellElement.setAttribute("data-index", index);
 
-    this.board.forEach((cell) => {
-      const cellElement = document.createElement("div");
-      cellElement.classList.add("game-board__cell");
-      cellElement.textContent = cell;
+    gameBoardField.appendChild(cellElement);
+  });
+}
 
-      gameBoardField.appendChild(cellElement);
-    });
-  },
+function toggleCurrentPlayer() {
+  if (!gameEnded) {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  } else {
+    // Если игра завершена, устанавливаем currentPlayer в победителя (если есть)
+    currentPlayer = winner || currentPlayer;
+  }
+}
 
-  toggleCurrentPlayer: function () {
-    this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
-  },
+function updateTurnMark() {
+  const turnMarkElement = document.querySelector(".game-board__turn-mark");
 
-  updateTurnMark: function () {
-    const turnMarkElement = document.querySelector(".game-board__turn-mark"); // Находит элемент с классом .game-board__turn-mark
-    if (turnMarkElement) {
-      turnMarkElement.textContent = this.currentPlayer; // Устанавливает текстовое содержимое элемента в текущую метку игрока (X или O)
-    }
-  },
+  if (turnMarkElement) {
+    // Определяем ссылку на иконку в зависимости от текущего игрока
+    const iconLink = currentPlayer === "X" ? "#icon-x" : "#icon-o";
 
-  handlePlayerMove: function () {
-    this.checkWinner(); // Проверяет, есть ли победитель после текущего хода
-    this.toggleCurrentPlayer(); // Меняет текущего игрока (X на O и наоборот)
-    this.updateTurnMark(); // Обновляет метку текущего игрока
-  },
+    // Устанавливаем новую ссылку на иконку
+    turnMarkElement
+      .querySelector("use")
+      .setAttribute("xlink:href", `./app/assets/icons.svg${iconLink}`);
+  }
+}
 
-  playerScore: 0,
-  player1Score: 0,
-  player2Score: 0,
-  tiesScore: 0,
-  cpuScore: 0,
+function handlePlayerMove() {
+  checkWinner();
+  toggleCurrentPlayer();
+  updateTurnMark();
+}
 
-  updateScores: function () {
-    const playerScoreElem = document.querySelector("#playerScore");
-    const tiesScoreElem = document.querySelector("#tiesScore");
-    const cpuScoreElem = document.querySelector("#cpuScore");
+function updateScores() {
+  const playerScoreElem = document.querySelector("#playerScore");
+  const tiesScoreElem = document.querySelector("#tiesScore");
+  const cpuScoreElem = document.querySelector("#cpuScore");
 
-    playerScoreElem.textContent = this.playerScore;
-    tiesScoreElem.textContent = this.tiesScore;
-    cpuScoreElem.textContent = this.cpuScore;
+  playerScoreElem.textContent = playerScore;
+  tiesScoreElem.textContent = tiesScore;
+  cpuScoreElem.textContent = cpuScore;
 
-    const player1ScoreElem = document.querySelector("#player1Score");
-    const player2ScoreElem = document.querySelector("#player2Score");
+  const player1ScoreElem = document.querySelector("#player1Score");
+  const player2ScoreElem = document.querySelector("#player2Score");
 
-    player1ScoreElem.textContent = this.player1Score;
-    player2ScoreElem.textContent = this.player2Score;
-  },
+  player1ScoreElem.textContent = player1Score;
+  player2ScoreElem.textContent = player2Score;
+}
 
-  checkWinner: function () {
-    const winPatterns = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8], // Горизонтальные линии
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8], // Вертикальные линии
-      [0, 4, 8],
-      [2, 4, 6], // Диагонали
-    ];
+function checkWinner() {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Горизонтальные линии
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Вертикальные линии
+    [0, 4, 8],
+    [2, 4, 6], // Диагонали
+  ];
 
-    for (const pattern of winPatterns) {
-      const [a, b, c] = pattern;
-      if (
-        this.board[a] &&
-        this.board[a] === this.board[b] &&
-        this.board[a] === this.board[c]
-      ) {
-        console.log("Игрок", this.currentPlayer, "побеждает!");
-        if (this.currentPlayer === "X") {
-          this.player1Score++;
-        } else if (this.currentPlayer === "O") {
-          this.player2Score++;
-        }
-        this.updateScores();
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      console.log("Игрок", currentPlayer, "побеждает!");
 
-        return;
+      winner = currentPlayer;
+
+      if (currentPlayer === "X") {
+        player1Score++;
+      } else if (currentPlayer === "O") {
+        player2Score++;
       }
+      gameEnded = true;
+
+      updateScores();
+
+      // Добавляем классы выигрышным ячейкам с задержкой
+      pattern.forEach((index, i) => {
+        setTimeout(() => {
+          const cell = document.querySelector(
+            `.game-board__cell[data-index="${index}"]`
+          );
+          cell.classList.add(`winner-${currentPlayer.toLowerCase()}`);
+        }, i * 500); // Задержка в миллисекундах между добавлениями классов
+      });
+
+      return pattern; // Возвращаем выигрышную комбинацию
     }
+  }
 
-    const isBoardFull = this.board.every((cell) => cell !== "");
-    if (isBoardFull) {
-      console.log("Игра окончена. Ничья!");
-      this.tiesScore++;
-      this.updateScores();
-    }
-  },
+  const isBoardFull = board.every((cell) => cell !== "");
+  if (isBoardFull) {
+    console.log("Игра окончена. Ничья!");
+    tiesScore++;
+    updateScores();
 
-  buttonX: document.querySelector(".game-menu__mark-btn--x"),
-  buttonO: document.querySelector(".game-menu__mark-btn--o"),
+    winner = null;
+    gameEnded = true;
+  }
 
-  init: function () {
-    this.buttonX.addEventListener("click", () => this.choosePlayerAndMark("X"));
-    this.buttonO.addEventListener("click", () => this.choosePlayerAndMark("O"));
-  },
+  // Если никто не выиграл, возвращаем null
+  return null;
+}
 
-  vsPlayerButton: document.querySelector(".game-menu__start-btn--vs-player"),
+function init() {
+  const buttonX = document.querySelector(".game-menu__mark-btn--x");
+  const buttonO = document.querySelector(".game-menu__mark-btn--o");
 
-  startVsPlayer: function () {
-    this.vsPlayerButton.addEventListener("click", () => this.startNewRound());
-  },
+  buttonX.addEventListener("click", () => choosePlayerAndMark("X"));
+  buttonO.addEventListener("click", () => choosePlayerAndMark("O"));
+  gameBoard.style.display = "none";
 
-  gameBoard: document.querySelector(".game-board"),
-  menu: document.querySelector(".game-menu"),
+  const yourScore = (document.querySelector(
+    ".game-board__player-score"
+  ).style.display = "none");
+  const cpuScore = (document.querySelector(
+    ".game-board__cpu-score"
+  ).style.display = "none");
+}
 
-  startNewRound: function () {
-    this.gameBoard.style.display = "flex";
-    this.menu.style.display = "none";
+const vsPlayerButton = document.querySelector(
+  ".game-menu__start-btn--vs-player"
+);
 
-    const yourScore = (document.querySelector(
-      ".game-board__player-score"
-    ).style.display = "none");
-    const cpuScore = (document.querySelector(
-      ".game-board__cpu-score"
-    ).style.display = "none");
+function startVsPlayer() {
+  vsPlayerButton.addEventListener("click", () => startNewRound());
+}
 
-    this.renderBoard();
+function toggleDisplay() {
+  const menu = document.querySelector(".game-menu");
 
-    if (this.player1Mark === "O") {
-      this.toggleCurrentPlayer();
-    }
+  if (gameBoard.style.display === "none") {
+    gameBoard.style.display = "flex";
+    menu.style.display = "none";
+  } else {
+    gameBoard.style.display = "none";
+    menu.style.display = "flex";
+  }
+}
 
-    // Удаляем существующие обработчики событий перед добавлением новых
-    const cells = document.querySelectorAll(".game-board__cell");
-    cells.forEach((cell) => {
-      cell.innerHTML = ""; // Очистить содержимое ячейки
-      cell.removeEventListener("click", this.handleCellClick);
-      cell.addEventListener("click", this.handleCellClick.bind(this));
-    });
-  },
+function startNewRound() {
+  // Проверяем, завершена ли игра
+  if (gameEnded) {
+    console.log("Игра завершена. Нельзя начать новый раунд.");
+    return;
+  }
 
-  handleCellClick: function (event) {
-    const cell = event.target;
-    const index = Array.from(cell.parentNode.children).indexOf(cell);
+  toggleDisplay();
+  renderBoard();
 
-    // Проверка, занята ли уже ячейка
-    if (this.board[index] === "") {
-      cell.innerHTML = this.currentPlayer;
-      this.board[index] = this.currentPlayer;
-      console.log("Игрок", this.currentPlayer, "ставит метку на клетку", index); // Выводит в консоль информацию о ходе игрока
-      this.handlePlayerMove();
-    } else {
-      console.log("Ячейка уже занята!");
-      // Можно добавить дополнительную логику, если ячейка уже занята (например, вывод сообщения)
-    }
-  },
+  if (player1Mark === "O") {
+    toggleCurrentPlayer();
+  }
 
-  nextRoundBtn: document.querySelector(".modal__next-round"),
+  const cells = document.querySelectorAll(".game-board__cell");
 
-  nextRound: function () {
-    this.nextRoundBtn.addEventListener("click", () => {
-      const cells = document.querySelectorAll(".game-board__cell");
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+    cell.removeEventListener("click", handleCellClick);
+    cell.addEventListener("click", handleCellClick);
+  });
+}
 
-      this.board = Array(9).fill("");
+const handleCellClick = (event) => {
+  if (gameEnded) {
+    console.log("Игра завершена. Нельзя поставить метку.");
+    return;
+  }
 
-      this.renderBoard(); // Очистить поле для нового раунда
-      this.currentPlayer = "X"; // Вернуть текущего игрока к начальному значению
-      this.updateTurnMark(); // Обновить метку текущего игрока
+  const cell = event.target;
+  const index = Array.from(cell.parentNode.children).indexOf(cell);
 
-      game.startVsPlayer();
-    });
-  },
+  if (board[index] === "") {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "64");
+    svg.setAttribute("height", "64");
+
+    const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    use.setAttribute(
+      "xlink:href",
+      `./app/assets/icons.svg#icon-${currentPlayer.toLowerCase()}`
+    );
+
+    svg.appendChild(use);
+
+    cell.innerHTML = ""; // Очищаем содержимое ячейки перед добавлением SVG
+    cell.appendChild(svg);
+
+    cell.classList.add(`played-${currentPlayer.toLowerCase()}`);
+
+    board[index] = currentPlayer;
+    console.log("Игрок", currentPlayer, "ставит метку на клетку", index);
+    handlePlayerMove();
+  } else {
+    console.log("Ячейка уже занята!");
+  }
 };
 
-game.init();
-game.startVsPlayer();
-game.nextRound();
+const nextRoundBtn = document.querySelector(".modal__next-round");
+
+function nextRound() {
+  nextRoundBtn.addEventListener("click", () => {
+    const cells = document.querySelectorAll(".game-board__cell");
+    board = Array(9).fill("");
+    renderBoard();
+    currentPlayer = winner || "X";
+    updateTurnMark();
+    startVsPlayer();
+  });
+}
+
+const restartBtn = document.querySelector(".game-board__button--restart");
+
+function restartGame() {
+  board = Array(9).fill("");
+
+  const cells = document.querySelectorAll(".game-board__cell");
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+    cell.classList.remove("played-x", "played-o", "winner-x", "winner-o");
+  });
+
+  currentPlayer = winner || "X";
+  updateTurnMark();
+
+  gameEnded = false;
+}
+
+restartBtn.addEventListener("click", restartGame);
+
+const logoElement = document.querySelector(".game-board__info .custom-logo");
+
+logoElement.addEventListener("click", () => {
+  toggleDisplay();
+  restartGame();
+});
+
+init();
+startVsPlayer();
+nextRound();
